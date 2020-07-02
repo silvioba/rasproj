5dd0ee7e6286b2be3915164ab16bf5178999f813
silviobarandun <barandun.silvio@gmail.com>
2020-07-02 14:21:15 +0200

    Interactive graphs working example

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import plotly.express as px
 
+
 ############################################
 # General Annotations
 ############################################
@@ -17,8 +18,8 @@ import plotly.express as px
 # PRE:  date: datetime, date of measurement.
 # POST: returns string with file name of data of given feature on given date
 def file_name(date):
-    path = ''
-    name = 'datalog_' + date.strftime("%d_%m_%Y") + '.csv'
+    path = 'data/'
+    name = path + 'datalog_' + date.strftime("%d_%m_%Y") + '.csv'
     return name
 
 
@@ -29,9 +30,10 @@ def file_name(date):
 def import_data(day_start, day_end):
     delta_days = day_end - day_start
     interval_days = []
-    for d in range(delta_days.days+1):
+    for d in range(delta_days.days + 1):
         interval_days.append(day_start + timedelta(days=d))
     # Create list of file names
+    print(interval_days)
     file_names = []
     for day in interval_days:
         file_names.append(file_name(day))
@@ -41,11 +43,11 @@ def import_data(day_start, day_end):
         data.append(pd.read_csv(filename))
     df = data[0]
     # Union in single panda
-    for dfs in data[0:]:
-        df.append(dfs, ignore_index=True)
+    for dfs in data[1:]:
+        df = df.append(dfs, ignore_index=True)
     # Convert times stamp to datetime format
-    df = df.rename(columns={"Unix_Timestamp": "Date_time"})
-    df.Date_time = pd.to_datetime(df.Date_time, unit='s')
+    df = df.rename(columns={"d_m_Y_H_M_S": "Date_time"})
+    df.Date_time = pd.to_datetime(df.Date_time, format="%d_%m_%Y_%H_%M_%S")
     return df
 
 
@@ -64,16 +66,39 @@ def return_dataset_ready(dataframe, period_measurement, time_interval_start, fea
 
 # PRE:  df: panda, initial data with a 'time' column
 #       feature: str, name of column of y-Axis,
-# POST: returns str
-def name_htmldiv(dataframe,feature):
+# POST: returns str of name file
+def name_htmldiv_single_graph(dataframe, feature):
+    starttsp = int(datetime.timestamp(dataframe.Date_time.iloc[0]))
+    endtsp = int(datetime.timestamp(dataframe.Date_time.iloc[-1]))
+    name = 'graphs_html_prep/html_div' + feature + '_' + str(starttsp) + '_to_' + str(endtsp) + '.html'
+    return name
 
 
 # PRE:  dataframe: panda, initial data with a 'time' column
 #       feature: str, name of column of y-Axis,
-# POST: returns panda with data
-def output_htmldiv_single_graph(dataframe,feature,name):
+#       name: str, want to use output of
+# POST: creates html output
+def output_htmldiv_single_graph(dataframe, feature):
+    filename = name_htmldiv_single_graph(dataframe, feature)
     fig = px.line(dataframe, x="Date_time", y=feature, title='Temperature')
-    fig.write_html(file=name, include_plotlyjs='plotly.js',full_html=False, auto_open=False)
+    fig.write_html(file=filename, include_plotlyjs='plotly.js', full_html=False, auto_open=False)
+
+
+# PRE:  name: str, name of output file
+#       *name_graphs: name of the graphs to be included,
+# POST:
+def create_html_page(name, *names_graphs):
+    # open final file in write mode
+    with open(name, 'w') as outputfile:
+        # open header in read mode and write in output
+        with open('basepage_top.html', 'r') as header:
+            outputfile.write(header.read())
+        for graph_name in names_graphs:
+            with open(graph_name, 'r') as graph:
+                outputfile.write(graph.read())
+        with open('basepage_bottom.html', 'r') as bottom:
+            outputfile.write(bottom.read())
+
 
 '''
 # Import data
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timedelta
-
+import numpy as np
 import functions as fn
 
-test_date = datetime(year=2020, month=6, day=26)
-df = fn.import_data(test_date, test_date)
-print(df)
+test_date1 = datetime(year=2020, month=7, day=2)
+test_date2 = datetime(year=2020, month=7, day=3)
+df = fn.import_data(test_date1, test_date2)
+fn.output_htmldiv_single_graph(df, 'Temperature1')
+fn.output_htmldiv_single_graph(df, 'Temperature2')
+fn.create_html_page('html_output/test_123.html',
+                    fn.name_htmldiv_single_graph(df, 'Temperature1'),
+                    fn.name_htmldiv_single_graph(df, 'Temperature2')
+                    )
